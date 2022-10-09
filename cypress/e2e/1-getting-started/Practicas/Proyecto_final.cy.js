271e8273c25668d96ba2b22fea568f823662b0dc
QAZebass <ze.basszender@gmail.com>
2022-10-09 00:28:51 -0300

    Actualizado

--- cypress/e2e/1-getting-started/Practicas/Proyecto_final.cy.js
+++ cypress/e2e/1-getting-started/Practicas/Proyecto_final.cy.js
@@ -31,7 +31,7 @@ describe('Page Object Model',()=>{
         cy.visit('/');  
     })
 
-    it('Desarrollo Proyecto',()=>{
+    it('Log In, Compra y Checkeo de precios',()=>{
         
         registerpage.GotoLogin();
         
