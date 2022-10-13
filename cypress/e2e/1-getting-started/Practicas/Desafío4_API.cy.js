5ffe4e0d927ec2272e6bb16940b849b48b47b81c
QAZebass <ze.basszender@gmail.com>
2022-10-13 01:00:22 -0300

    Desafío 4

---
+++ cypress/e2e/1-getting-started/Practicas/Desafío4_API.cy.js
@@ -0,0 +1,53 @@
+describe('Challenge 4/ API Testing',()=>{
+
+    const number= Math.floor(Math.random()*10000);
+    const user= `zebass${number}`;
+    const pass= 'sebas1233!'
+    const sex= 'male';
+    const day= '21';
+    const month= 'june';
+    const year= '1985';
+
+
+    it('Registering new user',()=>{
+        cy.request({
+            url: 'https://pushing-it-backend.herokuapp.com/api/register',
+            method: 'POST',
+            body: {
+                username: user,
+                password: pass,
+                gender: sex,
+                day: day,
+                month: month,
+                year:year,
+            }
+        }).then(response=>{
+            cy.log(response);
+            expect(response.body.newUser.username).equal(user);
+            
+        }).then(response=>{
+            cy.request({
+                url: 'https://pushing-it-backend.herokuapp.com/api/login',
+                method: 'POST',
+                body:{
+                    username: user,
+                    password: pass
+                }
+            }).then(response=>{
+                cy.log(response);
+                assert.equal(response.body.user.username, user)
+            
+            }).then(response=>{
+                cy.request({
+                    url:'https://pushing-it-backend.herokuapp.com/api/deleteuser/{usuario}',
+                    method: 'DELETE',
+                    failOnStatusCode: false
+
+                }).then(response=>{
+                    assert.equal(response.status, 404)
+                    cy.log(response)
+                })
+            })
+        })
+    })
+})
